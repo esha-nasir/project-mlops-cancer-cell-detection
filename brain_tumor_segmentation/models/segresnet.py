e30b3ea3c82600334839935125fa95f12465c6de
Esha Nasir <68076390+esha-nasir@users.noreply.github.com>
2025-06-07 17:34:01 +0300

    Update segresnet.py

--- brain_tumor_segmentation/models/segresnet.py
+++ brain_tumor_segmentation/models/segresnet.py
@@ -1,8 +1,16 @@
-from monai.networks.nets import SegResNet
 import torch
+from monai.networks.nets import SegResNet
+
 
 def get_model(config) -> SegResNet:
     """Create and return SegResNet model based on configuration."""
+    print("-------------------->To check model")
+    print("blocks_down ####--", config.blocks_down)
+    print("locks_up ###--", config.blocks_up)
+    print("init_filters ###--", config.init_filters)
+    print("in_channels ###--", config.in_channels)
+    print("out_channels ###--", config.out_channels)
+    print("dropout_prob ###--", config.dropout_prob)
     return SegResNet(
         blocks_down=config.blocks_down,
         blocks_up=config.blocks_up,
@@ -11,6 +19,7 @@ def get_model(config) -> SegResNet:
         out_channels=config.out_channels,
         dropout_prob=config.dropout_prob,
     )
+
 
 def load_model_from_checkpoint(config, checkpoint_path: str) -> SegResNet:
     """Load model from checkpoint."""
