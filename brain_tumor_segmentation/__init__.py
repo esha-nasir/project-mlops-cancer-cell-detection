b036b6e1b8f16e53885fd8334ec2258d71a4dcc0
Esha Nasir <68076390+esha-nasir@users.noreply.github.com>
2025-06-07 17:32:25 +0300

    Update __init__.py

--- brain_tumor_segmentation/__init__.py
+++ brain_tumor_segmentation/__init__.py
@@ -7,15 +7,14 @@ from .data.dataset import get_datasets, get_val_dataset
 from .models.segresnet import get_model, load_model_from_checkpoint
 from .training.trainer import Trainer
 from .utils.logger import WandBLogger
-from .utils.visualization import log_data_samples, log_predictions
+from .utils.visualization import log_data_samples
 
 __all__ = [
-    'get_datasets',
-    'get_val_dataset',
-    'get_model',
-    'load_model_from_checkpoint',
-    'Trainer',
-    'WandBLogger',
-    'log_data_samples', 
-    'log_predictions'
+    "get_datasets",
+    "get_val_dataset",
+    "get_model",
+    "load_model_from_checkpoint",
+    "Trainer",
+    "WandBLogger",
+    "log_data_samples",
 ]
