c29b8153ac58e0af48be34bd7fec0c1abe84b68d
Esha Nasir <68076390+esha-nasir@users.noreply.github.com>
2025-06-07 17:34:30 +0300

    Update dataset.py

--- brain_tumor_segmentation/data/dataset.py
+++ brain_tumor_segmentation/data/dataset.py
@@ -1,13 +1,30 @@
 import os
+
+import dvc.api
 from monai.apps import DecathlonDataset
-from monai.data import DataLoader
+
+
+def download_data(config):
+    """Download dataset using DVC."""
+    dataset_path = os.path.join(config.dataset_dir, "Task01_BrainTumour")
+    if not os.path.exists(dataset_path) or not os.listdir(dataset_path):
+        print("Pulling dataset with DVC...")
+        dvc.api.get_url(
+            url="dvc.yaml",  # Adjust to your DVC configuration
+            repo=".",  # Local or remote repository
+            path=dataset_path,
+        )
+    else:
+        print("Dataset already exists locally.")
+
 
 def get_datasets(config, train_transform, val_transform):
+    download_data(config)
     task_folder = os.path.join(config.dataset_dir, "Task01_BrainTumour", "imagesTr")
     should_download = not os.path.exists(task_folder) or not os.listdir(task_folder)
-    print("---------Config-------",config.num_workers)
-    print("---------train_transform-------",train_transform)
-    print("---------val_transform-------",val_transform)
+    print("---------Config-------", config.num_workers)
+    print("---------train_transform-------", train_transform)
+    print("---------val_transform-------", val_transform)
     train_dataset = DecathlonDataset(
         root_dir=config.dataset_dir,
         task="Task01_BrainTumour",
@@ -28,24 +45,12 @@ def get_datasets(config, train_transform, val_transform):
         num_workers=config.num_workers,
     )
 
-    # train_loader = DataLoader(
-    #     train_dataset,
-    #     batch_size=config.batch_size,
-    #     shuffle=True,
-    #     num_workers=config.num_workers,
-    # )
-
-    # val_loader = DataLoader(
-    #     val_dataset,
-    #     batch_size=config.batch_size,
-    #     shuffle=False,
-    #     num_workers=config.num_workers,
-    # )
     print("Type of train_dataset:", type(train_dataset))
     return train_dataset, val_dataset
 
 
 def get_val_dataset(config, transform):
+    download_data(config)
     return DecathlonDataset(
         root_dir=config.dataset_dir,
         task="Task01_BrainTumour",
